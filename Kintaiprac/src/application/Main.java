d0def7131874ef7437497449eeff718c431517af
b19k5006 <b19k5006@HTS-155-1502.tcs-one.local>
2020-11-10 11:34:01 +0900

    ？

--- Kintaiprac/src/application/Main.java
+++ Kintaiprac/src/application/Main.java
@@ -3,7 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 
@@ -11,7 +11,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("stamp.fxml"));
+			Pane root = (Pane)FXMLLoader.load(getClass().getResource("Login.fxml"));
 			Scene scene = new Scene(root,620,437);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
