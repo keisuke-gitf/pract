c6cf718f351d35b8107e14a6d326f4e8eb76c672
b20a0363 <b20a0363@HTS-155-2001.tcs-one.local>
2020-11-10 10:25:25 +0900

    login

--- Kintaiprac/src/application/Main.java
+++ Kintaiprac/src/application/Main.java
@@ -1,6 +1,7 @@
 package application;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
@@ -10,7 +11,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane() ;
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Login.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
